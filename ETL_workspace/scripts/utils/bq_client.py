bbc821eb68f2e938a7cc4b049099cbbf4c9bf6fd
Rex <chanminglung90@gmail.com>
2025-03-31 06:37:52 +0800

    refactor(etl): modify structure

--- ETL_workspace/scripts/utils/bq_client.py
+++ ETL_workspace/scripts/utils/bq_client.py
@@ -3,6 +3,8 @@ from google.cloud import bigquery
 # this methods needs set credentials in environment variable:
 # (powershell) $env:GOOGLE_APPLICATION_CREDENTIALS="path/to/your/service-account-file.json"
 # (check credentials) echo $env:GOOGLE_APPLICATION_CREDENTIALS
+
+
 def get_bigquery_client():
     client = bigquery.Client()
     return client
